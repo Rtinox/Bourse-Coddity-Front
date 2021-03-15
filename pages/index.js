2b10aaf7ad2a49f1e027c68b5b1787a866b86e9d
Rtinox <36574958+Rtinox@users.noreply.github.com>
2021-03-15 14:59:56 +0100

    home page

--- pages/index.js
+++ pages/index.js
@@ -5,59 +5,31 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Bourse Coddity</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.headercontainer}>
+        <div className={styles.headeritems}>
+          <p>Le site de la source informative !</p>
         </div>
+        <div className={styles.headeritems}>
+          <button className={styles.loginButton}>Se connecter</button>
+        </div>
+      </div>
+
+      <main className={styles.main}>
+        <h3>Que recherchez-vous ?</h3>
+        <input type="text" placeholder="Phrase, mots, ..." className={styles.inputbar}></input>
+        <button className={styles.transparentBtn}><img src="./search.png" alt="my image" width="30px"/></button>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Bourse coddity 2021 - Par _Rtinox et PJGame
         </a>
       </footer>
     </div>
